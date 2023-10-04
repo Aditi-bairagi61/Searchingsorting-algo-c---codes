74a9525f238ec889f866a14fdb78d9beb1535b70
https://github.com/Aditi-bairagi61 <aditibairagi1456@gmail.com>
2023-10-04 21:49:33 +0530

    Added linear_search.cpp file

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,30 +1,32 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-    int a[3],n,i,index;
-    cout<<"Enter the array elements:";
-    for(i=0;i<3;i++)
-    {
-        cin>>a[i];
+
+int main() {
+    int a[3], n, i;
+    cout << "Enter the array elements:";
+    for (i = 0; i < 3; i++) {
+        cin >> a[i];
     }
-    cout<<"Your array elements are:";
-    for(i=0;i<3;i++)
-    {
-        cout<<"\n"<<a[i]<<"\n";
+    cout << "Your array elements are:";
+    for (i = 0; i < 3; i++) {
+        cout << "\n" << a[i] << "\n";
     }
-    cout<<"Which element from above array you want to search:";
-    cin>>n;
-    for(i=0;i<3;i++)
-    {
-        if(a[i]==n)
-        {
-            cout<<"element is found"<<"\n";
-            index=i;
-            break;
+    cout << "Which element from above array you want to search:";
+    cin >> n;
+
+    bool found = false;
+
+    cout << "Your search element is found at positions/indices:";
+    for (i = 0; i < 3; i++) {
+        if (a[i] == n) {
+            cout << i << " ";
+            found = true;
         }
-     
     }
-    cout<<"Your serach element is found in:"<<""<<index <<" "<<"location";
+
+    if (!found) {
+        cout << "Element not found";
+    }
+
     return 0;
 }
